76aed1088b2cf3f59da2272333ce57d22b90bfd7
Jeffrey <jtorresa@co.ibm.com>
2017-10-01 16:22:39 -0500

    Comment summary

--- sentiment-analysis/batch_analyze.py
+++ sentiment-analysis/batch_analyze.py
@@ -3,6 +3,7 @@
 from polarity import type_file_enum
 from polarity import process_list as SentimentAnalysis
 from optparse import OptionParser
+from enum import Enum
 import pymongo
 import csv
 import datetime
@@ -13,6 +14,7 @@ pages = db.pages
 posts = db.posts
 reactions = db.reactions
 comments = db.comments
+results = db.results
 
 class Post:
     def __init__(self, post):
@@ -34,6 +36,12 @@ class Comment:
     def __eq__(self, other):
         return self.comment['_id'] == other.comment['_id']
     
+    def __eq__(self, other):
+        return self.comment['like_count'] > other.comment['like_count']
+
+    def __lt__(self, other):
+        return self.comment['like_count'] < other.comment['like_count']
+    
     def __hash__(self):
         return hash(self.comment['_id'])
 
@@ -54,16 +62,47 @@ def read_knowledge_base(file):
         reader = csv.reader(csvfile)
         words = {}
         for row in reader:
-            words[row[0]] = True if row[1] == 'true' else False
-            #words += row[0]
-            #new_row = [row[0], self.delete_accents(row[0]), self.delete_special_characters(self.delete_accents(row[0])),
-                    #self._spanis_stemmer.stem(self.delete_accents(self.delete_accents(row[0]))), row[1]]
-            #self._polarity.append(new_row)
+            if row[2] not in words:
+                words[row[2]] = {'synonyms': []}
+            words[row[2]]['synonyms'].append({'word': row[0], 'match_exact': True if row[1] == 'true' else False})
     #print(words)
     return words
+
+def get_all_comments_for(entity, match_exact=False, stored=False):
+    global comments
+    regex = {}
+    if match_exact:
+        regex = {'$regex': '.*\\b' + entity + '\\b.*'}
+    else:
+        regex = {'$regex': '.*' + entity + '.*', '$options': 'i'}
+
+    comments_for_entity = comments.find({ '$and': [
+        {'message': regex},
+        {'stored': {'$exists': stored}}
+    ]})
     
 
-def get_comments_for(entity, match_exact=False, with_polarity=False):
+    comments_for_entity = list(comments_for_entity)
+    comments_for_entity = [Comment(c) for c in comments_for_entity]
+    comments_set = set()
+
+    if comments_for_entity:
+        comments_set.update(comments_for_entity)
+
+    return comments_set
+
+class Options(Enum):
+    ALL = 1
+    POLARITY = 2
+    NO_POLARITY = 3
+    STORED = 4
+    NOT_STORED = 5
+    POLARITY_AND_STORED = 6
+    POLARITY_AND_NOT_STORED = 7
+    NO_POLARITY_AND_STORED = 8
+    NO_POLARITY_AND_NOT_STORED = 8
+
+def get_comments_for(entity, match_exact=False, opts=Options.ALL): #, with_polarity=False, stored=False):
     global comments
     regex = {}
     if match_exact:
@@ -72,21 +111,64 @@ def get_comments_for(entity, match_exact=False, with_polarity=False):
         regex = {'$regex': '.*' + entity + '.*', '$options': 'i'}
     
     comments_for_entity = {}
-
-    if with_polarity:
+    if opts == Options.ALL:
+        comments_for_entity = comments.find({ '$and': [
+            {'message': regex}
+        ]})
+    elif opts == Options.POLARITY:
         comments_for_entity = comments.find({ '$and': [
             {'message': regex},
             {'polarity': {'$exists': True}}
         ]})
-    else:
+    elif opts == Options.NO_POLARITY:
         comments_for_entity = comments.find({ '$and': [
             {'message': regex},
             {'polarity': {'$exists': False}}
         ]})
-
+    elif opts == Options.STORED:
+        comments_for_entity = comments.find({ '$and': [
+            {'message': regex},
+            {'stored': {'$exists': True}}
+        ]})
+    elif opts == Options.NOT_STORED:
+        comments_for_entity = comments.find({ '$and': [
+            {'message': regex},
+            {'stored': {'$exists': False}}
+        ]})
+    elif opts == Options.POLARITY_AND_STORED:
+        comments_for_entity = comments.find({ '$and': [
+            {'message': regex},
+            {'polarity': {'$exists': True}},
+            {'stored': {'$exists': True}}
+        ]})
+    elif opts == Options.POLARITY_AND_NOT_STORED:
+        comments_for_entity = comments.find({ '$and': [
+            {'message': regex},
+            {'polarity': {'$exists': True}},
+            {'stored': {'$exists': False}}
+        ]})
+    elif opts == Options.NO_POLARITY_AND_STORED:
+        comments_for_entity = comments.find({ '$and': [
+            {'message': regex},
+            {'polarity': {'$exists': False}},
+            {'stored': {'$exists': True}}
+        ]})
+    elif opts == Options.NO_POLARITY_AND_NOT_STORED:
+        comments_for_entity = comments.find({ '$and': [
+            {'message': regex},
+            {'polarity': {'$exists': False}},
+            {'stored': {'$exists': False}}
+        ]})
+    
     comments_for_entity = list(comments_for_entity)
+    #print("comments_for_entity =>", comments_for_entity)
     comments_for_entity = [Comment(c) for c in comments_for_entity]
-    return comments_for_entity
+    comments_set = set()
+
+    if comments_for_entity:
+        comments_set.update(comments_for_entity)
+
+    return comments_set
 
 def get_posts_for(entity, match_exact=False):
     global posts
@@ -105,24 +187,34 @@ def get_posts_for(entity, match_exact=False):
 
     posts_for_entity = list(posts_for_entity)
     posts_for_entity = [Post(p) for p in posts_for_entity]
-    return posts_for_entity
+
+    posts_set = set()
+
+    if posts_for_entity:
+        posts_set.update(posts_for_entity)
+    return posts_set
 
 def get_reactions_for(entity, match_exact=False):
     global posts
     global reactions
-    posts_for_entity = get_posts_for(entity, match_exact)
+    posts_set = get_posts_for(entity, match_exact)
     reactions_for_entity = []
-    for p in posts_for_entity:
+    for p in posts_set:
         reactions_for_entity.extend(list(reactions.find({'_id': p.post['_id']})))
 
     reactions_for_entity = [Reaction(r) for r in reactions_for_entity]
-    return reactions_for_entity
+
+    reactions_set = set()
+    # TODO: if it doesn't work then: reactions_for_entity = list(get_reactions_for(key, match_exact)) before if
+    if reactions_for_entity:
+        reactions_set.update(reactions_for_entity)
+    return reactions_set
 
 def update_comments_with_polarity(comments_set):
     for c in comments_set:
         updated_comment = dict(c.comment)
         updated_comment['polarity'] = analyzer.process_text(c.comment['message'])['Polarity']
-        del updated_comment['_id']
+        #del updated_comment['_id']
         
         print(c.comment['_id'], "=>", updated_comment)
         comments.update(c.comment, updated_comment, upsert=True)
@@ -150,7 +242,7 @@ def get_posts_comments_reactions_set(knowledge_base):
         
     return (posts_set, comments_set, reactions_set)
 
-def write_comments_to_file(name):
+def write_comments_to_file(comments_set):
     file = '../results/comments/' + name + '-' + datetime.datetime.today().strftime('%Y-%m-%d') + '.csv'
     #comments_set = get_comments_for(name, )
     with open(file, 'w+', newline='') as csvfile:
@@ -159,12 +251,6 @@ def write_comments_to_file(name):
             csvwriter.writerow([c.comment['_id'], c.comment['message'], c.comment['like_count'], c.comment['polarity'], c.comment['created_time']])
 
 if __name__ == '__main__':
-
-    #print("Palabras corrupcion", palabras_corrupcion)
-    #print("Casos corrupcion", casos_corrupcion)
-    #print("Instituciones", instituciones)
-    #print("Lideres opinion", lideres_opinion)
-    #print("Partidos politicos", partidos_politicos)
 
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="file", default="lexicons/politico.csv", help="name of the file with the lexicons")
@@ -175,10 +261,84 @@ if __name__ == '__main__':
     analyzer.load_list(type_file_enum.polarity, options.file, options.sep)
 
     palabras_corrupcion = read_knowledge_base('../base-conocimiento/palabras-corrupcion.txt')
+    #print("Palabras corrupcion", palabras_corrupcion)
     casos_corrupcion = read_knowledge_base('../base-conocimiento/casos-corrupcion.txt')
+    #print("Casos corrupcion", casos_corrupcion)
     instituciones = read_knowledge_base('../base-conocimiento/instituciones.txt')
+    #print("Instituciones", instituciones)
     lideres_opinion = read_knowledge_base('../base-conocimiento/lideres-opinion.txt')
+    #print("Lideres", lideres_opinion)
     partidos_politicos = read_knowledge_base('../base-conocimiento/partidos-politicos.txt')
+    #print("Partidos", partidos_politicos)
+
+    for name, value in lideres_opinion.items():
+        for s in value['synonyms']:
+            # TODO: instead of looking for each synonym, simply put them all in an $or inside the query
+            # Look for comments
+            print(s)
+            if 'comments' not in lideres_opinion[name]:
+                lideres_opinion[name]['comments'] = set()
+            
+            # TODO: instead of looking for comments "with" and "without" polarity, look for them all and then filter them
+            # Buscar comentarios que todavia no se han summarized 
+            comments_set = get_comments_for(s['word'], s['match_exact'], Options.NO_POLARITY)
+            #print("comments_set =>", comments_set)
+            lideres_opinion[name]['comments'] |= comments_set
+            
+        for c in lideres_opinion[name]['comments']:
+            print("Comments with NO POLARITY of", name, "=>", c.comment)
+        update_comments_with_polarity(lideres_opinion[name]['comments'])
+
+
+    for name, value in lideres_opinion.items():
+        comments_set = set()
+        for s in value['synonyms']:
+            comments_set |= get_comments_for(s['word'], s['match_exact'], Options.POLARITY_AND_NOT_STORED)
+
+        for c in comments_set:
+            print("comments with polarity and not stored =>", c.comment)
+            updated_comment = dict(c.comment)
+            updated_comment['stored'] = True
+            date = c.comment['created_time'].strftime('%Y_%m_%d')
+
+            res = results.find_one({'_id': name})
+            if res is None:
+                res = {
+                    '_id': name,
+                    'comment_summary': {},
+                    'post_summary': {},
+                    'reaction_summary': {}
+                }
+            
+            if not date in res['comment_summary']:
+                res['comment_summary'][date] = {}
+                res['comment_summary'][date]['positive'] = 0
+                res['comment_summary'][date]['negative'] = 0
+                res['comment_summary'][date]['neutral'] = 0
+                res['comment_summary'][date]['comment_most_likes'] = ''
+                res['comment_summary'][date]['like_count'] = 0
+                res['comment_summary'][date]['comment_id'] = ''
+
+
+            if c.comment['like_count'] > res['comment_summary'][date]['like_count'] or res['comment_summary'][date]['comment_most_likes'] == '':
+                res['comment_summary'][date]['comment_most_likes'] = c.comment['message']
+                res['comment_summary'][date]['like_count'] = c.comment['like_count']
+                res['comment_summary'][date]['comment_id'] = c.comment['_id']
+            
+            #print("updated_comment =>", updated_comment)
+
+            if updated_comment['polarity'] > 0:
+                res['comment_summary'][date]['positive'] += 1
+            elif updated_comment['polarity'] < 0:
+                res['comment_summary'][date]['negative'] += 1
+            else:
+                res['comment_summary'][date]['neutral'] += 1
+
+            print("res =>", res)
+            results.update({'_id': name}, res, upsert=True)
+            comments.update(c.comment, updated_comment)
+    
+
 
     """
     print(analyzer.process_text("Me gusta la nueva ley de ciencia innovación y tecnologia, Pero algo anda mal  ? "))
@@ -192,28 +352,3 @@ if __name__ == '__main__':
     print(analyzer.process_text("Malditos perros pena de muerte que mas queremos ver Juan Orlando cuando la pena de muerte ya esto se salio de las manos"))
     """
     
-    (posts_set, comments_set, reactions_set) = get_posts_comments_reactions_set(lideres_opinion)
-
-    update_comments_with_polarity(comments_set)
-    
-    write_comments_to_file("claudia-lopez")
-    
-        
-        #for p in posts_corrupcion:
-            #print(p.post)
-        
-        #for c in comments_corrupcion:
-            #print(c.comment)
-        
-        #for r in reactions_corrupcion:
-            #print(r.reaction)
-
-        #print(c['message'], "=>", analyzer.process_text(c['message'])['Label'])
-    
-
-    # TODO: leer entidades de un archivo y no con un string como se esta haciendo ahora
-    #comments = get_comments_for("álvaro Uribe")
-    #
-
-    #print(comments.count())
-    #print(comments[0])
