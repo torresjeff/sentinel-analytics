a549f94cc5f4a32bd5a07d41c1e6add17176ed56
Jeffrey <jtorresa@co.ibm.com>
2017-10-19 01:17:34 -0500

    Fix TODO

--- descriptive/batch_describe.py
+++ descriptive/batch_describe.py
@@ -57,4 +57,4 @@ if __name__ == '__main__':
     # TODO: grafico de cantidad de posts sobre el personaje que se esta viendo
     # TODO: actividad del lider (cuantas publicaciones ha hecho en su pagina) - en caso de que tenga pagina
     # TODO: mostrar un map(?) de cuales regiones se publican mas sobre corrupcion
-    # TODO: mostrar un map(?) de cuales regiones son mas AFECTADAS por corrupcion. Esto sería el gráfico, pero buscando municipios/departamentos/pais en vez de cualquier palabra asociada
+    # TODO: mostrar un map(?) de cuales regiones son mas AFECTADAS por corrupcion. Esto sería el grafo, pero buscando municipios/departamentos/pais en vez de cualquier palabra asociada
